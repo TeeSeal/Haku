49eb47eacb483ebce5372605a01132c8b6f37dee
TeeSeal <alex.theoddor@gmail.com>
2017-09-14 16:19:58 +0300

    Minor renames

--- src/commands/util/exec.js
+++ src/commands/util/exec.js
@@ -1,18 +1,18 @@
 const { Command } = require('discord-akairo');
-const exec = require('util').promisify(require('child_process').exec);
+const execute = require('util').promisify(require('child_process').exec);
 
-function run(msg, args) {
+function exec(msg, args) {
   const { command } = args;
   if (!command) return msg.util.error('give me something to run.');
 
-  return exec(command).then(({ stdout, stderr }) => {
+  return execute(command).then(({ stdout, stderr }) => {
     return msg.util.send(stderr || stdout, { code: true });
   }).catch(err => {
     return msg.util.send(err, { code: true });
   });
 }
 
-module.exports = new Command('exec', run, {
+module.exports = new Command('exec', exec, {
   aliases: ['exec'],
   ownerOnly: true,
   description: 'execute a command in the terminal.',
