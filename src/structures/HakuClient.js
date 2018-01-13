3d7b9893456f1e122720849b7e2728a4407da4c2
TeeSeal <teesealz@gmail.com>
2018-01-13 16:59:30 +0200

    Refactor Haku client

--- src/structures/HakuClient.js
+++ src/structures/HakuClient.js
@@ -1,4 +1,4 @@
-const akairo = require('discord-akairo')
+const { AkairoClient, CommandUtil } = require('discord-akairo')
 const logr = require('logr')
 const keychain = require('../../keychain')
 
@@ -6,7 +6,7 @@ const SequelizeDatabase = require('../db/SequelizeDatabase')
 const InventoryHandler = require('./items/InventoryHandler')
 const MusicHandler = require('./music/MusicHandler')
 
-class HakuClient extends akairo.AkairoClient {
+class HakuClient extends AkairoClient {
   constructor(opts) {
     if (!opts.database) throw new Error('please specify a database.')
     super(opts)
@@ -16,33 +16,31 @@ class HakuClient extends akairo.AkairoClient {
     this.music = null
   }
 
-  init() {
-    logr.info('Connecting to database...')
-    this.db
-      .init()
-      .then(db => {
-        logr.success('OK')
+  async init() {
+    try {
+      logr.info('Connecting to database...')
+      await this.db.init()
+      logr.success('OK')
 
-        logr.info('Setting up inventories...')
-        this.inventories = new InventoryHandler(db.users)
-        logr.success('OK')
+      logr.info('Setting up inventories...')
+      this.inventories = new InventoryHandler(this.db.users)
+      logr.success('OK')
 
-        logr.info('Setting up music...')
-        this.music = new MusicHandler(keychain)
-        logr.success('OK')
+      logr.info('Setting up music...')
+      this.music = new MusicHandler(keychain)
+      logr.success('OK')
 
-        logr.info('Logging in...')
-        this.login(keychain.token)
-      })
-      .catch(err => {
-        throw err
-      })
+      logr.info('Logging in...')
+      this.login(keychain.token)
+    } catch (err) {
+      throw err
+    }
   }
 }
 
 module.exports = HakuClient
 
-Object.assign(akairo.CommandUtil.prototype, {
+Object.assign(CommandUtil.prototype, {
   info(content, opts) {
     const name = this.message.member
       ? this.message.member.displayName
